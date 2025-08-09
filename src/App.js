67cd6ef66aba620b5880829d6dfbd117b11c429d
Leshan21 <leshanpasindu12@gmail.com>
2025-08-09 16:10:27 +0530

    basic

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  const handleNameChange = () =>{
+    const names = ['bob', 'kavin', 'dave'];
+    const int = Math.floor(Math.random()* 3)
+    return names[int];
+  }
+
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
