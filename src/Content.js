6702e8e01331a9c59c24a6c79e9b9ecfba3c2324
Leshan21 <leshanpasindu12@gmail.com>
2025-08-10 10:15:49 +0530

    also click events

--- src/Content.js
+++ src/Content.js
@@ -11,10 +11,22 @@ export const Content = () => {
     const handleClick = () => {
       console.log('Button clicked');
     }
+
+    const handleClick2 = (name) => {
+      console.log(`${name} was clicked`);
+    }
+    const handleClick3 = (e) => {
+      console.log(e.target.innerText);
+    }
+
   return (
     <main>
         <p>Hello {handleNameChange()} </p>
         <button onClick={handleClick}>click it</button>
+        <button onClick={() => {
+          handleClick2('bob');
+        }}>click it</button>
+        <button onClick={(e) => {handleClick3(e)}}>click it</button>
     </main>
   )
 }
