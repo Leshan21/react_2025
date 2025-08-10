b8d7717881baf5d50fd41cbf9dd52a3b452b5d84
Leshan21 <leshanpasindu12@gmail.com>
2025-08-10 10:07:19 +0530

    simple click event

--- src/Content.js
+++ src/Content.js
@@ -7,9 +7,14 @@ export const Content = () => {
         const int = Math.floor(Math.random() * 3)
         return names[int];
     }
+
+    const handleClick = () => {
+      console.log('Button clicked');
+    }
   return (
     <main>
         <p>Hello {handleNameChange()} </p>
+        <button onClick={handleClick}>click it</button>
     </main>
   )
 }
