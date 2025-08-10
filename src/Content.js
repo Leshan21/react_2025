4509acb5a2856413609e89e16b754bc204516dd6
Leshan21 <leshanpasindu12@gmail.com>
2025-08-10 11:45:12 +0530

    render list

--- src/Content.js
+++ src/Content.js
@@ -21,12 +21,19 @@ export const Content = () => {
        },
     ])
 
+    const handleCheck = (id) => 
+    {
+      const listitems = items.map((item) => item.id === id ? {...item, checked: !item.checked} : item)
+      setItems(listitems)
+    } 
   return (
     <main>
       <ul>
         {items.map((item) => (
           <li className="item" key={item.id}>
-            <input type="checkbox" checked={item.checked} />
+            <input type="checkbox" onChange={() => {
+              handleCheck(item.id)
+            }} checked={item.checked} />
             <label>{item.item}</label>
             <FaTrashAlt role="button" tabIndex="0" />
           </li>
