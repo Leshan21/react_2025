c71b1d15b6e678f3a904d69536f3b3c1c1ec934b
Leshan21 <leshanpasindu12@gmail.com>
2025-08-13 19:09:16 +0530

    build simple eventhandle function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,11 +9,16 @@ function counter() {
     const renderTags = () => {
       if(obj.tags.length === 0 ) return <p>There are no tags !</p>;
       return <ul>{obj.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
-    }  
+    }
+    
+    const handleEvnt = () => {
+      console.log("Button clicked");
+    }
   return (
    <>
     {obj.tags.length === 0 && "Please create a new tag !"} 
     {renderTags()}
+    <button onClick={handleEvnt} className='btn btn-secondary btn-sm'>btn</button>
    </>
   )
 }
