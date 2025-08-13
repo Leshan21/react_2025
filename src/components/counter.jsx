066a39d6777f32bc46e64e425a614603faa01fcc
Leshan21 <leshanpasindu12@gmail.com>
2025-08-13 18:37:56 +0530

    clear the code for learn conditional rendaring

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,36 +5,13 @@ function counter() {
         count: 0,
         tag : ['tag1', 'tag2', 'tag3']
     };
-
-  let classes = getBadgeClasses();
-
-  const formatCount = () => {
-    return obj.count === 0 ? "Zero" : obj.count;
-  }
   return (
-    <div>
-
-      <span style={{
-        fontSize: 30,
-        fontWeight: 'bold',
-
-      }} className={classes}>{formatCount()}</span>
-      <button className='btn btn-secondary btn-sm'>Increment</button>
+    <div> 
       <ul>
-        {obj.tag.map(tag => <li>{tag}</li>)}
+        {obj.tag.map(tag => <li key={tag}>{tag}</li>)}
       </ul>
     </div>
   )
-
-    function getBadgeClasses() {
-        let classes = "badge m-2";
-        if (obj.count === 0) {
-            classes += " bg-warning";
-        } else {
-            classes += " bg-primary";
-        }
-        return classes;
-    }
 }
 
 export default counter
