f7992579b7c1d6138a3158c2e50e6337ef372bf9
Leshan21 <leshanpasindu12@gmail.com>
2025-08-13 18:50:08 +0530

    conditional rendaring

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,14 +3,18 @@ import React from 'react'
 function counter() {
     const obj = {
         count: 0,
-        tag : ['tag1', 'tag2', 'tag3']
+        tags : []
     };
+
+    const renderTags = () => {
+      if(obj.tags.length === 0 ) return <p>There are no tags !</p>;
+      return <ul>{obj.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+    }  
   return (
-    <div> 
-      <ul>
-        {obj.tag.map(tag => <li key={tag}>{tag}</li>)}
-      </ul>
-    </div>
+   <>
+    {obj.tags.length === 0 && "Please create a new tag !"} 
+    {renderTags()}
+   </>
   )
 }
 
